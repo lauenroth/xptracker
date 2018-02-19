b109d22c7854609a8ed1ec1da99457949ee7ae63
Jörg Lauenroth <jorg.lauenroth@inthepocket.mobi>
2018-02-19 14:48:07 +0100

    Change password

---
+++ src/components/InfoPopup.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="info-popup" v-bind:class="{ show: showPopup }">{{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: 'InfoPopup',
+  props: ['message'],
+  data() {
+    return {
+      showPopup: false,
+    };
+  },
+  watch: {
+    message() {
+      this.showPopup = true;
+      setTimeout(() => {
+        this.showPopup = false;
+      }, 3500);
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .info-popup {
+   background-color: rgb(0, 61, 25);
+   top: -150px;
+   color: #fff;
+   left: 0;
+   line-height: 30px;
+   padding: 10px;
+   position: fixed;
+   right: 0;
+   text-align: center;
+   transition: .3s top;
+   z-index: 30;
+ }
+ .info-popup.show {
+   top: 0;
+ }
+</style>
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -13,22 +13,49 @@
 
       <SaveableInput label="Currency" :value="currency" :options="currencies" v-on:onSaveValue="saveCurrency"></SaveableInput>
 
+      <a @click="changePasswordForm">Need to change your password?</a>
+
       <footer>
         <p class="version">Version: 1.0.0</p>
         <div class="btn" @click="logout">Logout</div>
       </footer>
     </div>
+    <form class="change-email" v-if="showConfirmPassword" v-on:submit.prevent="onConfirmEmailChange">
+      <div>
+        <label>Confirm with password</label>
+        <input type="password" v-model="currentPassword" autocomplete="current-password" minlength="6" required>
+        <div class="buttons">
+          <div class="btn cancel" @click="hideConformEmailChange">Cancel</div>
+          <button type="submit">Change e-mail</button>
+        </div>
+      </div>
+    </form>
+    <form class="change-password" v-if="showChangePassword" v-on:submit.prevent="onChangePassword">
+      <div>
+        <label>Current password</label>
+        <input type="password" v-model="currentPassword" autocomplete="current-password" minlength="6" required>
+        <label>New password</label>
+        <input type="password" v-model="newPassword" autocomplete="new-password" minlength="6" required>
+        <div class="buttons">
+          <div class="btn cancel" @click="hideChangePasswordForm">Cancel</div>
+          <button type="submit">Change password</button>
+        </div>
+      </div>
+    </form>
     <div class="error-popup" v-bind:class="{ show: showError }">{{currentError}}</div>
+    <InfoPopup :message="infoMessage"></InfoPopup>
   </section>
 </template>
 
 <script>
 import firebase from 'firebase';
 import SaveableInput from './SaveableInput';
+import InfoPopup from './InfoPopup';
 
 export default {
   name: 'Settings',
   components: {
+    InfoPopup,
     SaveableInput,
   },
   data() {
@@ -59,7 +86,14 @@ export default {
         { name: 'ZAR', value: 'R' },
       ],
       currentError: 'TEST',
+      currentPassword: '',
+      infoMessage: 'Password has been changed',
+      newPassword: '',
+      newEmail: '',
       showError: false,
+      showInfo: true,
+      showChangePassword: false,
+      showConfirmPassword: false,
     };
   },
   methods: {
@@ -74,12 +108,36 @@ export default {
         displayName: newName,
       }).then(() => {
         this.name = newName;
+        this.infoMessage = 'Name has been updated';
       });
     },
     saveEmail(email) {
+      this.showConfirmPassword = true;
+      this.newEmail = email;
+    },
+    hideConformEmailChange() {
+      this.currentPassword = '';
+      this.showConfirmPassword = false;
+    },
+    onConfirmEmailChange() {
       const user = firebase.auth().currentUser;
-      user.updateEmail(email).then(() => {
-        this.email = email;
+      const credential = firebase.auth.EmailAuthProvider.credential(
+        this.email,
+        this.currentPassword,
+      );
+
+      user.reauthenticateWithCredential(credential).then(() => {
+        user.updateEmail(this.newEmail).then(() => {
+          this.infoMessage = 'E-mail has been updated';
+          this.showConfirmPassword = false;
+          this.currentPassword = '';
+        }).catch((error) => {
+          this.currentError = error.message;
+          this.showError = true;
+          setTimeout(() => {
+            this.showError = false;
+          }, 3500);
+        });
       }).catch((error) => {
         this.currentError = error.message;
         this.showError = true;
@@ -91,6 +149,43 @@ export default {
     saveCurrency(currency) {
       window.localStorage.setItem('currency', currency);
       this.currency = currency;
+      this.infoMessage = 'Currency has been updated';
+    },
+    changePasswordForm() {
+      this.showChangePassword = true;
+    },
+    hideChangePasswordForm() {
+      this.currentPassword = '';
+      this.newPassword = '';
+      this.showChangePassword = false;
+    },
+    onChangePassword() {
+      const user = firebase.auth().currentUser;
+      const credential = firebase.auth.EmailAuthProvider.credential(
+        this.email,
+        this.currentPassword,
+      );
+
+      user.reauthenticateWithCredential(credential).then(() => {
+        user.updatePassword(this.newPassword).then(() => {
+          this.showChangePassword = false;
+          this.currentPassword = '';
+          this.newPassword = '';
+          this.infoMessage = 'Password has been updated';
+        }).catch((error) => {
+          this.currentError = error.message;
+          this.showError = true;
+          setTimeout(() => {
+            this.showError = false;
+          }, 3500);
+        });
+      }).catch((error) => {
+        this.currentError = error.message;
+        this.showError = true;
+        setTimeout(() => {
+          this.showError = false;
+        }, 3500);
+      });
     },
   },
   mounted() {
@@ -127,12 +222,8 @@ export default {
     padding-top: 10px;
   }
 
-  label {
-    padding: 0 10px;
-  }
-
-  input {
-    padding: 0 10px;
+  a {
+    text-align: center;
   }
 
   .btn.save {
@@ -146,6 +237,41 @@ export default {
 
   footer {
     margin-top: auto;
+  }
+
+  form {
+    background-color: rgba(0, 25, 61, .9);
+    bottom: 0;
+    display: flex;
+    left: 0;
+    position: fixed;
+    right: 0;
+    top: 0;
+    z-index: 20;
+  }
+  form > div {
+    background: #fff;
+    margin: auto;
+    width: 320px;
+  }
+  form label,
+  form input {
+    box-sizing: border-box;
+    display: block;
+    margin: 0;
+    padding: 0 10px;
+  }
+  form label {
+    margin-top: 10px;
+  }
+  .buttons button {
+    float: left;
+    width: 66%;
+  }
+  .buttons .cancel {
+    background-color:#aaa;
+    float: left;
+    width: 34%;
   }
 </style>
 
